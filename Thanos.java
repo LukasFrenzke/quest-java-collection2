8b48ca00884e71a7db3f1a9cc6fc169bcdf88ec2
LukasFrenzke <lukas.frenzke@telekom.de>
2022-03-08 16:00:25 +0100

    Comparable and Comparator added

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -12,14 +14,25 @@ public class Thanos {
         heroes.add(new Hero("Thor", 1501));
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
-
+        Collections.sort(heroes);
 
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
-
-
+        Comparator<Hero> heroAgeComparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero h1, Hero h2) {
+                if(h1.getAge() < h2.getAge()) {
+                    return 1;
+                } else if (h1.getAge() > h2.getAge()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+        Collections.sort(heroes, heroAgeComparator);
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
